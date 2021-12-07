a463cf2ec63eec127313bef4b0c1163416bc5bf2
Louis-Marie <louismarie.merminod@gmail.com>
2021-12-07 15:03:33 +0100

    Test all user input interpretation

--- src/tests/UserInputTest.java
+++ src/tests/UserInputTest.java
@@ -35,4 +35,36 @@ public class UserInputTest {
         assertEquals("Learn Rust", instruction.parameter());
     }
 
+    @Test
+    public void testUserInputInterpretationRemoveAction() {
+        IOInterface io = new IOFakeImpl("- 1");
+        String input = io.readInput();
+        Instruction instruction = interpreter.interpret(input);
+        assertEquals(Action.REMOVE, instruction.action());
+    }
+
+    @Test
+    public void testUserInputInterpretationDoneAction() {
+        IOInterface io = new IOFakeImpl("x 1");
+        String input = io.readInput();
+        Instruction instruction = interpreter.interpret(input);
+        assertEquals(Action.DONE, instruction.action());
+    }
+
+    @Test
+    public void testUserInputInterpretationTodoAction() {
+        IOInterface io = new IOFakeImpl("o 1");
+        String input = io.readInput();
+        Instruction instruction = interpreter.interpret(input);
+        assertEquals(Action.TODO, instruction.action());
+    }
+
+    @Test
+    public void testUserInputInterpretationExitAction() {
+        IOInterface io = new IOFakeImpl("q");
+        String input = io.readInput();
+        Instruction instruction = interpreter.interpret(input);
+        assertEquals(Action.EXIT, instruction.action());
+    }
+
 }
